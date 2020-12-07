867a0d9ff2ae820a0280de702da8dd8d8863e824
aepsaefulloh <aepsaefulloh1396@gmail.com>
2020-12-07 22:43:59 +0700

    cardProduct

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>
-    <div class="row mt-4">
-      <div class="col">
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-align-center align-self-center">
+              <h2><strong>Delicious Food Menu,</strong><br />in Your Gadget</h2>
+              <p>Ayo segera pilih makanan favorit anda</p>
+              <button class="btn btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/img/hero/hero.png" width="100%" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/img/hero/hero.png" width="100%" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6">
         <div class="d-flex h-100">
           <div class="justify-align-center align-self-center">
             <h2><strong>Delicious Food Menu,</strong><br />in Your Gadget</h2>
@@ -11,9 +37,6 @@
             </button>
           </div>
         </div>
-      </div>
-      <div class="col">
-        <img src="../assets/img/hero.png" width="100%" alt="" />
       </div>
     </div>
   </div>

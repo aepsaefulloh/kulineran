a3260bd3277ce72843f04e81de2e5f77598d5448
aepsaefulloh <aepsaefulloh1396@gmail.com>
2020-12-07 16:25:01 +0700

    navbar & Hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="row mt-4">
+      <div class="col">
+        <div class="d-flex h-100">
+          <div class="justify-align-center align-self-center">
+            <h2><strong>Delicious Food Menu,</strong><br />in Your Gadget</h2>
+            <p>Ayo segera pilih makanan favorit anda</p>
+            <button class="btn btn-success">
+              <b-icon-arrow-right></b-icon-arrow-right> Pesan
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <img src="../assets/img/hero.png" width="100%" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
